ed68c823e569451eac661d26308417fee24df154
villatore <villatore@yahoo.com.mx>
2014-02-11 10:02:47 -0600

    agregue validación de forma datos personales

--- js/datospersonalesForm.js
+++ js/datospersonalesForm.js
@@ -1,174 +1,190 @@
 function MuestraEntrega(){
-    if (!$ge('mismodomv').checked)
-        expandElem('domicilioEntregav')
-    else
-        collapseElem('domicilioEntregav')
+    if (!$ge('mismodomv').checked) {
+        expandElem('domicilioEntregav');
+        $ge('domicilioEntregav').value = $ge('domiciliov').value
+    } else
+        collapseElem('domicilioEntregav');
 }
 
-		function collapseElem(obj)
-		{
-			var el = document.getElementById(obj);
-			el.style.display = 'none';
-			el.style.visibility = 'hidden';
-			
+function collapseElem(obj){
+	var el = document.getElementById(obj);
+	el.style.display = 'none';
+	el.style.visibility = 'hidden';
+}
+
+function expandElem(obj){
+	var el = document.getElementById(obj);
+	el.style.display = '';
+	el.style.visibility = '';
+}
+
+// collapse all elements, except the first one
+function collapseAll()
+{
+	var numFormPages = 1;
+
+	for(i=2; i <= numFormPages; i++)
+	{
+		currPageId = ('mainForm_' + i);
+		collapseElem(currPageId);
+	}
+}
+
+function validateField(fieldId, fieldType, required)
+{
+	fieldObj = document.getElementById(fieldId);
+
+	if(fieldType == 'text'  ||  fieldType == 'textarea'  ||  fieldType == 'password'  ||  fieldType == 'file'  ||  fieldType == 'phone'  || fieldType == 'website')
+	{
+	    if (required == 1 && fieldObj.value == '') {
+	        fieldObj.setAttribute("class", "mainFormError");
+	        fieldObj.setAttribute("className", "mainFormError");
+	        fieldObj.focus();
+	        return false;
+	    } else {
+	        fieldObj.setAttribute("class", "mainForm");
+	        fieldObj.setAttribute("className", "");
+	    }
+	}
+	else if(fieldType == 'menu'  || fieldType == 'country'  || fieldType == 'state')
+	{	
+		if(required == 1 && fieldObj.selectedIndex == 0)
+		{				
+			fieldObj.setAttribute("class","mainFormError");
+			fieldObj.setAttribute("className","mainFormError");
+			fieldObj.focus();
+			return false;					
 		}
 
+	}
+	else if(fieldType == 'email')
+	{	
+		if((required == 1 && fieldObj.value=='')  ||  (fieldObj.value!=''  && !validate_email(fieldObj.value)))
+		{				
+			fieldObj.setAttribute("class","mainFormError");
+			fieldObj.setAttribute("className","mainFormError");
+			fieldObj.focus();
+			return false;					
+	    } else {
+	        fieldObj.setAttribute("class", "mainForm");
+	        fieldObj.setAttribute("className", "");
+	    }
+	}
+	return true;
+}
 
-		function expandElem(obj)
+function validate_email(emailStr){		
+	apos=emailStr.indexOf("@");
+	dotpos=emailStr.lastIndexOf(".");
+
+	if (apos<1||dotpos-apos<2) 
+		return false;
+	else
+		return true;
+}
+
+
+function validateDate(fieldId, fieldBoxId, fieldType, required,  minDateStr, maxDateStr)
+{
+	retValue = true;
+
+	fieldBox = document.getElementById(fieldBoxId);
+	fieldObj = document.getElementById(fieldId);	
+	dateStr = fieldObj.value;
+
+
+	if(required == 0  && dateStr == '')
+		return true;
+
+
+	if(dateStr.charAt(2) != '/'  || dateStr.charAt(5) != '/' || dateStr.length != 10)
+	{
+		retValue = false;
+	}	
+
+	else	// format's okay; check max, min
+	{
+		currDays = parseInt(dateStr.substr(0,2),10) + parseInt(dateStr.substr(3,2),10)*30  + parseInt(dateStr.substr(6,4),10)*365;
+		//alert(currDays);
+
+		if(maxDateStr != '')
 		{
-			var el = document.getElementById(obj);
-			el.style.display = '';
-			el.style.visibility = '';
+			maxDays = parseInt(maxDateStr.substr(0,2),10) + parseInt(maxDateStr.substr(3,2),10)*30  + parseInt(maxDateStr.substr(6,4),10)*365;
+			//alert(maxDays);
+			if(currDays > maxDays)
+				retValue = false;
 		}
 
-			// collapse all elements, except the first one
-			function collapseAll()
-			{
-				var numFormPages = 1;
+		if(minDateStr != '')
+		{
+			minDays = parseInt(minDateStr.substr(0,2),10) + parseInt(minDateStr.substr(3,2),10)*30  + parseInt(minDateStr.substr(6,4),10)*365;
+			//alert(minDays);
+			if(currDays < minDays)
+				retValue = false;
+		}
+	}
 
-				for(i=2; i <= numFormPages; i++)
-				{
-					currPageId = ('mainForm_' + i);
-					collapseElem(currPageId);
-				}
-			}
+	if(retValue == false)
+	{
+		fieldObj.setAttribute("class","mainFormError");
+		fieldObj.setAttribute("className","mainFormError");
+		fieldObj.focus();
+		return false;
+	}
+}
 
-			function validateField(fieldId, fieldType, required)
-			{
-				fieldObj = document.getElementById(fieldId);
+function validaPagina()
+{
+	retVal = true;
+	if (!validateField('nombrev', 'text', 1)) retVal=false;
+    if (!validateField('domiciliov', 'textarea', 1)) retVal=false;
+    if (!validateField('telv', 'phone', 1)) retVal=false;
+    if ($ge('mailv').value != '')
+        if (!validateField('mailv', 'email', 1)) retVal = false;
+    if (!$ge('mismodomv').checked)
+        if (!validateField('domicilioEntregav', 'textarea', 1)) retVal = false;
+//                if (validateField('field_4','fieldBox_4','checkbox',1) == false)
+//                 retVal=false;
+//                if (validateField('field_5','fieldBox_5','radio',1) == false)
+//                 retVal=false;
+//                if (validateDate('field_6','fieldBox_6','date',1,'','') == false)
+//                 retVal=false;
+//                if (validateField('field_7','fieldBox_7','password',1) == false)
+//                 retVal=false;
+//                if (validateField('field_8','fieldBox_8','file',1) == false)
+//                 retVal=false;
+//                if (validateField('field_9','fieldBox_9','email',1) == false)
+//                 retVal=false;
+//                if (validateField('field_10','fieldBox_10','phone',1) == false)
+//                 retVal=false;
+//                if (validateField('field_11','fieldBox_11','website',1) == false)
+//                 retVal=false;
+//                if (validateField('field_12','fieldBox_12','country',1) == false)
+//                 retVal=false;
+//                if (validateField('field_13','fieldBox_13','state',1) == false)
+//                 retVal=false;
 
-				if(fieldType == 'text'  ||  fieldType == 'textarea'  ||  fieldType == 'password'  ||  fieldType == 'file'  ||  fieldType == 'phone'  || fieldType == 'website')
-				{	
-					if(required == 1 && fieldObj.value == '')
-					{
-					    fieldObj.setAttribute("class", "mainFormError");
-						fieldObj.setAttribute("className","mainFormError");
-						fieldObj.focus();
-						return false;					
-					}
-				}
-				else if(fieldType == 'menu'  || fieldType == 'country'  || fieldType == 'state')
-				{	
-					if(required == 1 && fieldObj.selectedIndex == 0)
-					{				
-						fieldObj.setAttribute("class","mainFormError");
-						fieldObj.setAttribute("className","mainFormError");
-						fieldObj.focus();
-						return false;					
-					}
+	if(retVal == false)
+	{
+	    alert(EtiquetaReturn("#msjValidaForma"));
+		return false;
+	}
+	return retVal;
+}
+function Almacena() {
+    if (validaPagina()) {
+        var DatosPersonales = JSON.parse("{}");
+        DatosPersonales.nombrev = $ge('nombrev').value;
+        DatosPersonales.domiciliov = $ge('domiciliov').value;
+        DatosPersonales.domicilioEntregav = $ge('domicilioEntregav').value;
+        DatosPersonales.mailv = $ge('mailv').value;
+        DatosPersonales.telv = $ge('telv').value;
+        DatosPersonales.celv = $ge('celv').value;
 
-				}
-				else if(fieldType == 'email')
-				{	
-					if((required == 1 && fieldObj.value=='')  ||  (fieldObj.value!=''  && !validate_email(fieldObj.value)))
-					{				
-						fieldObj.setAttribute("class","mainFormError");
-						fieldObj.setAttribute("className","mainFormError");
-						fieldObj.focus();
-						return false;					
-					}
-				}
-			}
-
-			function validate_email(emailStr)
-			{		
-				apos=emailStr.indexOf("@");
-				dotpos=emailStr.lastIndexOf(".");
-
-				if (apos<1||dotpos-apos<2) 
-				{
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
-
-
-			function validateDate(fieldId, fieldBoxId, fieldType, required,  minDateStr, maxDateStr)
-			{
-				retValue = true;
-
-				fieldBox = document.getElementById(fieldBoxId);
-				fieldObj = document.getElementById(fieldId);	
-				dateStr = fieldObj.value;
-
-
-				if(required == 0  && dateStr == '')
-				{
-					return true;
-				}
-
-
-				if(dateStr.charAt(2) != '/'  || dateStr.charAt(5) != '/' || dateStr.length != 10)
-				{
-					retValue = false;
-				}	
-
-				else	// format's okay; check max, min
-				{
-					currDays = parseInt(dateStr.substr(0,2),10) + parseInt(dateStr.substr(3,2),10)*30  + parseInt(dateStr.substr(6,4),10)*365;
-					//alert(currDays);
-
-					if(maxDateStr != '')
-					{
-						maxDays = parseInt(maxDateStr.substr(0,2),10) + parseInt(maxDateStr.substr(3,2),10)*30  + parseInt(maxDateStr.substr(6,4),10)*365;
-						//alert(maxDays);
-						if(currDays > maxDays)
-							retValue = false;
-					}
-
-					if(minDateStr != '')
-					{
-						minDays = parseInt(minDateStr.substr(0,2),10) + parseInt(minDateStr.substr(3,2),10)*30  + parseInt(minDateStr.substr(6,4),10)*365;
-						//alert(minDays);
-						if(currDays < minDays)
-							retValue = false;
-					}
-				}
-
-				if(retValue == false)
-				{
-					fieldObj.setAttribute("class","mainFormError");
-					fieldObj.setAttribute("className","mainFormError");
-					fieldObj.focus();
-					return false;
-				}
-			}
-
-			function validaPagina()
-			{
-				retVal = true;
-				if (!validateField('nombrev', 'text', 1)) retVal=false;
-                if (!validateField('domiciliov', 'textarea', 1)) retVal=false;
-                if (validateField('field_3','fieldBox_3','menu',1) == false)
-                 retVal=false;
-                if (validateField('field_4','fieldBox_4','checkbox',1) == false)
-                 retVal=false;
-                if (validateField('field_5','fieldBox_5','radio',1) == false)
-                 retVal=false;
-                if (validateDate('field_6','fieldBox_6','date',1,'','') == false)
-                 retVal=false;
-                if (validateField('field_7','fieldBox_7','password',1) == false)
-                 retVal=false;
-                if (validateField('field_8','fieldBox_8','file',1) == false)
-                 retVal=false;
-                if (validateField('field_9','fieldBox_9','email',1) == false)
-                 retVal=false;
-                if (validateField('field_10','fieldBox_10','phone',1) == false)
-                 retVal=false;
-                if (validateField('field_11','fieldBox_11','website',1) == false)
-                 retVal=false;
-                if (validateField('field_12','fieldBox_12','country',1) == false)
-                 retVal=false;
-                if (validateField('field_13','fieldBox_13','state',1) == false)
-                 retVal=false;
-
-				if(retVal == false)
-				{
-					alert('Please correct the errors.  Fields marked with an asterisk (*) are required');
-					return false;
-				}
-				return retVal;
-			}
+        //se carga el carrito de la tienda, para poder agregarle
+        //var CarritoTienda = JSON.parse(localStorage.getItem(Partesurl[i]));
+        //window.localStorage.setItem(nombreCarrito, JSON.stringify(CarritoTienda));
+        alert(JSON.stringify(DatosPersonales));
+        alert("todo se guardoooo");
+    }
+}
--- js/lbl_es.js
+++ js/lbl_es.js
@@ -1,19 +1,25 @@
 var l8n = {
     "en-US": {
-        "#version": "Version 0.0.1en-Ene 31 2014",
+        "#version": "Version 0.0.1en-Copyright (C) Feb 4, 2014",
         "#ajustes": "Settings",
         "#per": "Rol",
         "#sal" : "Exit",
-        "#ttipnombre": "Tooltip test..."
+        "#ttipnombre": "Tooltip test...",
+        "#AyudaDatosPersonales": "Your data will be used for deliver or ticket",
+        "#saveForm": "Save",
+        "#msjValidaForma": "Please correct the errors.  Fields marked with red color are required"
     },
     "es": { //español
     },
     "es-419": { //español - latinoamerica
-        "#version": "Version 0.0.1es-Ene 31 2014",
+        "#version": "Version 0.0.1es- Copyright (C) Feb 4, 2014",
         "#ajustes": "Ajustes",
         "#per": "Perfil",
         "#sal" : "Salida",
-        "#ttipnombre": "Prueba del Tooltip..."
+        "#ttipnombre": "Prueba del Tooltip...",
+        "#AyudaDatosPersonales": "Sus datos son para facturación o entrega a domicilio",
+        "#saveForm": "Salvar",
+        "#msjValidaForma": "Por favor capture los datos requeridos, remarcados en rojo"
     },
     "ko": {
         ".email": "",
@@ -28,14 +34,19 @@ function Textos() {
     Etiqueta("#per");
     Etiqueta("#sal");
     Etiqueta("#ttipnombre");
+    Etiqueta("#AyudaDatosPersonales");
+    Etiqueta("#saveForm");
+    EtiquetaTag("footerT", "#version");
+    EtiquetaTag("footerT2", "#version");
+    EtiquetaTag("footerC", "#version");
 }
 function Etiqueta(key) {
     var lenguaje = "en-US"; //idioma por default
-    var i = 0;
     if (l8n[window.navigator.language])
         lenguaje = window.navigator.language;
 
     /*Probar esto
+    var i = 0;
     for (i = 0; size_object(l8n[lenguaje]); i++) {
     var key = l8n[lenguaje][i];
     document.querySelector(key).textContent = l8n[lenguaje][key];
@@ -43,14 +54,33 @@ function Etiqueta(key) {
 
     if (window.navigator && l8n[lenguaje]) {
         document.querySelector(key).textContent = l8n[lenguaje][key];
+        if (document.querySelector(key).type == "button")
+            document.querySelector(key).value = l8n[lenguaje][key];
+    }
+}
+function EtiquetaTag(control,key) {
+    var lenguaje = "en-US"; //idioma por default
+    if (l8n[window.navigator.language])
+        lenguaje = window.navigator.language;
+
+    if (window.navigator && l8n[lenguaje]) {
+        $ge(control).innerHTML = l8n[lenguaje][key];
+    }
+}
+function EtiquetaReturn(key) {
+    var lenguaje = "en-US"; //idioma por default
+    if (l8n[window.navigator.language])
+        lenguaje = window.navigator.language;
+
+    if (window.navigator && l8n[lenguaje]) {
+        return l8n[lenguaje][key];
     }
 }
 /**
- *
- * Returns an element specified by elementId. Similar to (but not exactly like) jQuery's $()
- *
- */
-function $ge(elementId)
-{
+*
+* Returns an element specified by elementId. Similar to (but not exactly like) jQuery's $()
+*
+*/
+function $ge(elementId){
   return document.getElementById(elementId);
 }
